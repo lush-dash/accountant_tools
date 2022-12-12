f7020ebdbe2f54748f1e32af222ba172bfef56d2
Daria Latushkina <loreleispirit@yandex.ru>
2022-12-12 17:55:49 +0300

    convertToWords tests added

--- client/src/redux/utils/convertToWords/convertToWords.test.js
+++ client/src/redux/utils/convertToWords/convertToWords.test.js
@@ -15,34 +15,46 @@ describe('Tests for convertToWords', () => {
       b = { number: '' };
       const resultA = await convertToWords(a);
       //   const resultB = await convertToWords(b);
-      expect(resultA).toBe('Ten pounds sterling 56 pence');
+      expect(resultA).toBe('Ten pounds 56 pence');
     //   expect(resultB).toBe('Ноль рублей 00 копеек');
     });
     test('Incorrect argument', async () => {
       a = { number: 'test', currency: 'test', language: 'test' };
       b = {};
       c = '';
-
       await expect(() => convertToWords(a)).rejects.toThrow('Invalid input');
-
-    //   expect(async () => {
-    //     const resultB = await convertToWords(b);
-    //     return resultB;
-    //   }).toThrow('Invalid input');
-    //   expect(async () => {
-    //     const resultC = await convertToWords(c);
-    //     return resultC;
-    //   }).toThrow('Invalid input');
+      await expect(() => convertToWords(b)).rejects.toThrow('Invalid input');
+      await expect(() => convertToWords(c)).rejects.toThrow('Invalid input');
     });
   });
-//   describe('Functional', () => {
-//     test('Numbers within borders', () => {
-//       a = 1.45;
-//       b = 456;
-//       c = 7.67;
-//       expect(convertToWords(a)).toBe('0.45');
-//       expect(convertToWords(b)).toBe('0.00');
-//       expect(convertToWords(c)).toBe('0.67');
-//     });
-//   });
+  describe('Functional', () => {
+    test('Numbers within borders', async () => {
+      a = { number: '1.45', currency: 'USD', language: 'eng' };
+      b = { number: '178.01', currency: 'GBP', language: 'eng' };
+      c = { number: '5', currency: 'EUR', language: 'eng' };
+      await expect(convertToWords(a)).resolves.toBe('One dollar 45 cents');
+      await expect(convertToWords(b)).resolves.toBe('One hundred seventy eight pounds 01 penny');
+      await expect(convertToWords(c)).resolves.toBe('Five euros 00 cents');
+    });
+    test('Numbers on the border', async () => {
+      a = { number: '0', currency: 'USD', language: 'eng' };
+      b = { number: '999999999999.99', currency: 'RUB', language: 'eng' };
+      await expect(convertToWords(a)).resolves.toBe('Zero dollars 00 cents');
+      await expect(convertToWords(b)).resolves.toBe('Nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine rubles 99 kopecks');
+    });
+    test('Numbers outside the border', async () => {
+      a = { number: '-5', currency: 'USD', language: 'eng' };
+      b = { number: '9999999999999.99', currency: 'RUB', language: 'eng' };
+      await expect(() => convertToWords(a)).rejects.toThrow('Invalid input');
+      await expect(() => convertToWords(b)).rejects.toThrow('Invalid input');
+    });
+    test.skip('Translator to Russian', async () => {
+      a = { number: '1.45', currency: 'USD', language: 'rus' };
+      b = { number: '178.01', currency: 'GBP', language: 'rus' };
+      c = { number: '5', currency: 'RUB', language: 'rus' };
+      await expect(convertToWords(a)).resolves.toBe('Один доллар 45 центов');
+      await expect(convertToWords(b)).resolves.toBe('Сто семьдесят восемь фунтов 01 пенни');
+      await expect(convertToWords(c)).resolves.toBe('Пять рублей 00 копеек');
+    });
+  });
 });
